a1a76a86e24686651a8f641a6f57174eeceff6a2
“Karl <iontexproject@gmail.com>
2022-06-03 23:29:00 +0530

    fixed issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-// add bootstrap css
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 
 import { useEffect } from "react";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -11,5 +12,24 @@ export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <React.Fragment>
+     <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-226942804-1`}
+      />
+
+      <Script id='google-analytics' strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-226942804-1', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
