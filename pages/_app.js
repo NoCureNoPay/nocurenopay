33cc3c5e3ac8e66d4143b450b519770c28871846
“Karl <iontexproject@gmail.com>
2022-08-03 14:20:35 +0530

    add  js

--- pages/_app.js
+++ pages/_app.js
@@ -29,6 +29,33 @@ export default function MyApp({ Component, pageProps }) {
             });
                 `}
       </Script>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-10941292230" />
+      <Script id='google-adds'>
+        {`
+          window.dataLayer = window.dataLayer || \[\];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10941292230');
+        `}
+        </Script>
+        <Script id='google-adds2'>
+          {`
+            document.addEventListener('click', function(e) {
+            if (e.target.matches('button\[type="submit"\] , \[type="submit"\] \*')) {
+            var timer = setInterval(function() {
+            if (document.querySelector('\[class="swal2-icon swal2-success swal2-icon-show"\]')) {
+            gtag('event', 'conversion', {
+            'send\_to': 'AW-10941292230/YqUZCIys\_MwDEMa9m-Eo'
+            });
+            clearInterval(timer);
+            }
+            }, 1000);
+            }
+            })
+          `}
+
+        </Script>
       <Component {...pageProps} />
     </React.Fragment>
   );
